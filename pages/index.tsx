003bc54421efcc9792f450a2e142e719892b3c2c
morganmarienelson <nelsonmm5047@uwec.edu>
2022-06-16 10:22:04 -0500

    work branch created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,5 @@
 import React from "react";
-import {
-  Button,
-  Cascader,
-  DatePicker,
-  Form,
-  Input,
-  InputNumber,
-  Radio,
-  Select,
-  Switch,
-  TreeSelect,
-} from "antd";
-import { useState } from "react";
+import { Button, Form, Input, Select } from "antd";
 const { Option } = Select;
 
 const App = () => {
