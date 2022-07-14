6dfaafebb20c5006c7701ed19b500f48e80b1b7f
morganmarienelson <nelsonmm5047@uwec.edu>
2022-07-14 08:51:51 -0500

    serving display laid out

--- pages/components/matchDataTable.tsx
+++ pages/components/matchDataTable.tsx
@@ -1,196 +1,196 @@
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHeader,
-  TableRow,
+    Table,
+    TableBody,
+    TableCell,
+    TableHeader,
+    TableRow,
 } from "grommet/components";
 import styles from "./css/matchStats.module.css";
 
 interface MatchDataTableProps {
-  state: any;
+    state: any;
 }
 
-const MatchDataTable: React.FC<MatchDataTableProps> = ({ state }) => {
-  const aces =
-    +JSON.stringify(state.context.acesOnFirstServe) +
-    +JSON.stringify(state.context.acesOnSecondServe);
+const MatchDataTable: React.FC<MatchDataTableProps> = ({state}) => {
+    const aces =
+        +JSON.stringify(state.context.acesOnFirstServe) +
+        +JSON.stringify(state.context.acesOnSecondServe);
 
-  const doubleFaults = +JSON.stringify(state.context.doubleFaults);
+    const doubleFaults = +JSON.stringify(state.context.doubleFaults);
 
-  const missedReturns = +JSON.stringify(state.context.missedReturns);
+    const missedReturns = +JSON.stringify(state.context.missedReturns);
 
-  const baselineWinners =
-    +JSON.stringify(state.context.pointsWonByForehandWinner) +
-    +JSON.stringify(state.context.pointsWonByBackhandWinner);
+    const baselineWinners =
+        +JSON.stringify(state.context.pointsWonByForehandWinner) +
+        +JSON.stringify(state.context.pointsWonByBackhandWinner);
 
-  const baselineUnforced =
-    +JSON.stringify(state.context.pointsLostByForehandUnforcedError) +
-    +JSON.stringify(state.context.pointsLostByBackhandUnforcedError);
+    const baselineUnforced =
+        +JSON.stringify(state.context.pointsLostByForehandUnforcedError) +
+        +JSON.stringify(state.context.pointsLostByBackhandUnforcedError);
 
-  const netWinners =
-    +JSON.stringify(state.context.pointsWonByForehandVolleyWinner) +
-    +JSON.stringify(state.context.pointsWonByBackhandVolleyWinner) +
-    +JSON.stringify(state.context.pointsWonByOverheadWinner);
+    const netWinners =
+        +JSON.stringify(state.context.pointsWonByForehandVolleyWinner) +
+        +JSON.stringify(state.context.pointsWonByBackhandVolleyWinner) +
+        +JSON.stringify(state.context.pointsWonByOverheadWinner);
 
-  const netUnforced =
-    +JSON.stringify(state.context.pointsLostByForehandVolleyUnforcedError) +
-    +JSON.stringify(state.context.pointsLostByBackhandVolleyUnforcedError) +
-    +JSON.stringify(state.context.pointsLostByOverheadUnforcedError);
+    const netUnforced =
+        +JSON.stringify(state.context.pointsLostByForehandVolleyUnforcedError) +
+        +JSON.stringify(state.context.pointsLostByBackhandVolleyUnforcedError) +
+        +JSON.stringify(state.context.pointsLostByOverheadUnforcedError);
 
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableCell scope="col" border="bottom">
-            Shot Type
-          </TableCell>
-          <TableCell scope="col" border="bottom">
-            Value
-          </TableCell>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Aces</div>
-          </TableCell>
-          <TableCell>{aces}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Double Faults</div>
-          </TableCell>
-          <TableCell>{doubleFaults}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Missed Returns</div>
-          </TableCell>
-          <TableCell>{missedReturns}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Baseline Winners</div>
-          </TableCell>
-          <TableCell>{baselineWinners}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Baseline Unforced Errors</div>
-          </TableCell>
-          <TableCell>{baselineUnforced}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Forehand Winners</div>
-          </TableCell>
-          <TableCell>
-            {+JSON.stringify(state.context.pointsWonByForehandWinner)}
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Forehand Unforced Errors</div>
-          </TableCell>
-          <TableCell>
-            {+JSON.stringify(state.context.pointsLostByForehandUnforcedError)}
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Backhand Winners</div>
-          </TableCell>
-          <TableCell>
-            {+JSON.stringify(state.context.pointsWonByBackhandWinner)}
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Backhand Unforced Errors</div>
-          </TableCell>
-          <TableCell>
-            {+JSON.stringify(state.context.pointsLostByBackhandUnforcedError)}
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Net Winners</div>
-          </TableCell>
-          <TableCell>{netWinners}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Net Unforced Errors</div>
-          </TableCell>
-          <TableCell>{netUnforced}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Forehand Volley Winners</div>
-          </TableCell>
-          <TableCell>
-            {+JSON.stringify(state.context.pointsWonByForehandVolleyWinner)}
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>
-              Forehand Volley Unforced Errors
-            </div>
-          </TableCell>
-          <TableCell>
-            {
-              +JSON.stringify(
-                state.context.pointsLostByForehandVolleyUnforcedError
-              )
-            }
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Backhand Volley Winners</div>
-          </TableCell>
-          <TableCell>
-            {" "}
-            {+JSON.stringify(state.context.pointsWonByBackhandVolleyWinner)}
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>
-              Backhand Volley Unforced Errors
-            </div>
-          </TableCell>
-          <TableCell>
-            {" "}
-            {
-              +JSON.stringify(
-                state.context.pointsLostByBackhandVolleyUnforcedError
-              )
-            }
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Overhead Winners</div>
-          </TableCell>
-          <TableCell>
-            {" "}
-            {+JSON.stringify(state.context.pointsWonByOverheadWinner)}
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <div className={styles.tableCell}>Overhead Unforced Errors</div>
-          </TableCell>
-          <TableCell>
-            {+JSON.stringify(state.context.pointsLostByOverheadUnforcedError)}
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
-  );
+    return (
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableCell scope="col" border="bottom">
+                        Shot Type
+                    </TableCell>
+                    <TableCell scope="col" border="bottom">
+                        Value
+                    </TableCell>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Aces</div>
+                    </TableCell>
+                    <TableCell>{aces}</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Double Faults</div>
+                    </TableCell>
+                    <TableCell>{doubleFaults}</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Missed Returns</div>
+                    </TableCell>
+                    <TableCell>{missedReturns}</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Baseline Winners</div>
+                    </TableCell>
+                    <TableCell>{baselineWinners}</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Baseline Unforced Errors</div>
+                    </TableCell>
+                    <TableCell>{baselineUnforced}</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Forehand Winners</div>
+                    </TableCell>
+                    <TableCell>
+                        {+JSON.stringify(state.context.pointsWonByForehandWinner)}
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Forehand Unforced Errors</div>
+                    </TableCell>
+                    <TableCell>
+                        {+JSON.stringify(state.context.pointsLostByForehandUnforcedError)}
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Backhand Winners</div>
+                    </TableCell>
+                    <TableCell>
+                        {+JSON.stringify(state.context.pointsWonByBackhandWinner)}
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Backhand Unforced Errors</div>
+                    </TableCell>
+                    <TableCell>
+                        {+JSON.stringify(state.context.pointsLostByBackhandUnforcedError)}
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Net Winners</div>
+                    </TableCell>
+                    <TableCell>{netWinners}</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Net Unforced Errors</div>
+                    </TableCell>
+                    <TableCell>{netUnforced}</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Forehand Volley Winners</div>
+                    </TableCell>
+                    <TableCell>
+                        {+JSON.stringify(state.context.pointsWonByForehandVolleyWinner)}
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>
+                            Forehand Volley Unforced Errors
+                        </div>
+                    </TableCell>
+                    <TableCell>
+                        {
+                            +JSON.stringify(
+                                state.context.pointsLostByForehandVolleyUnforcedError
+                            )
+                        }
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Backhand Volley Winners</div>
+                    </TableCell>
+                    <TableCell>
+                        {" "}
+                        {+JSON.stringify(state.context.pointsWonByBackhandVolleyWinner)}
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>
+                            Backhand Volley Unforced Errors
+                        </div>
+                    </TableCell>
+                    <TableCell>
+                        {" "}
+                        {
+                            +JSON.stringify(
+                                state.context.pointsLostByBackhandVolleyUnforcedError
+                            )
+                        }
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Overhead Winners</div>
+                    </TableCell>
+                    <TableCell>
+                        {" "}
+                        {+JSON.stringify(state.context.pointsWonByOverheadWinner)}
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell scope="row">
+                        <div className={styles.tableCell}>Overhead Unforced Errors</div>
+                    </TableCell>
+                    <TableCell>
+                        {+JSON.stringify(state.context.pointsLostByOverheadUnforcedError)}
+                    </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+    );
 };
 
 export default MatchDataTable;
--- pages/components/servingData.tsx
+++ pages/components/servingData.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import {Meter} from "grommet";
-import {Grommet, TableBody, TableCell, TableHeader, TableRow, Table} from "grommet/components";
-import {Card, Col, Progress, Row, Statistic} from "antd";
+import {Grommet, TableBody, TableCell, TableHeader, TableRow, Table, Chart, Box, Heading} from "grommet/components";
+import styles from "./css/serveDataPanel.module.css";
+import {Progress} from "antd";
 
 interface ServingDataProps {
     state: any;
@@ -12,8 +13,13 @@ const ServingData: React.FC<ServingDataProps> = ({state}) => {
         +JSON.stringify(state.context.pointsWonOnServe) +
         +JSON.stringify(state.context.pointsLostOnServe);
 
-    const percentWonOnServe =
+    const totalPoints = totalServePoints + +JSON.stringify(state.context.pointsWonOnReturn) +
+        +JSON.stringify(state.context.pointsLostOnReturn);
+
+    const percentOfServingPointsWon =
         +((+JSON.stringify(state.context.pointsWonOnServe) / (totalServePoints)) * 100).toPrecision(2);
+
+    const percentOfTotalPointsWonOnServe = +((+JSON.stringify(state.context.pointsWonOnServe) / (totalServePoints)) * 100).toPrecision(2);
 
     const firstServePercentage =
         +((+JSON.stringify(state.context.madeFirstServes) / (totalServePoints)) * 100).toPrecision(2);
@@ -22,58 +28,167 @@ const ServingData: React.FC<ServingDataProps> = ({state}) => {
 
     const secondServePercentage = +((+JSON.stringify(state.context.madeSecondServes) / (totalHitSecondServes)) * 100).toPrecision(2);
 
+    const totalNotReturnedServes =
+        +JSON.stringify(state.context.notReturnedFirstServes) +
+        +JSON.stringify(state.context.notReturnedSecondServes) +
+        +JSON.stringify(state.context.acesOnSecondServe)
+        + +JSON.stringify(state.context.acesOnFirstServe);
+
+    const percentNotReturnedServesToAlley = +(+JSON.stringify(state.context.notReturnedServesToAlley) / totalNotReturnedServes * 100).toPrecision(2);
+
+    const percentNotReturnedServesToCenter = +(+JSON.stringify(state.context.notReturnedServesToCente) / totalNotReturnedServes * 100).toPrecision(2);
+
+    const percentNotReturnedServesToBody = +(+JSON.stringify(state.context.notReturnedServesToBody) / totalNotReturnedServes * 100).toPrecision(2);
+
+    const checkSuccess = (value: number) => {
+        if (value >= 75) {
+            return "#69E53B";
+        } else if (value > 50) {
+            return " #EEFB46";
+        } else {
+            return "red";
+        }
+    };
+
     return (
         <div>
             <h1>Serving Statistics</h1>
-            <Row gutter={16}>
-                <Col span={12}>
-                    <Card>
-                        <Statistic
-                            title="Percentage of Points Won on Serve"
-                            value={percentWonOnServe}
-                            valueStyle={{
-                                color: '#3f8600',
-                            }}
-                            suffix="%"
+            <div className={styles.row}>
+                <div className={styles.servePercentages}>
+                    <h4>Percent of Total Points Won On Serve</h4>
+                    <h1>{percentOfTotalPointsWonOnServe}%</h1>
+                </div>
+                <div className={styles.servePercentages}>
+                    <h4>Percent of Serving Points Won</h4>
+                    <h1>{percentOfServingPointsWon}%</h1>
+                </div>
+                <div className={styles.servePercentages}>
+                    <h4>First Serve Percentage</h4>
+                    <h1>{firstServePercentage}%</h1>
+                </div>
+                <div className={styles.servePercentages}>
+                    <h4>Second Serve Percentage</h4>
+                    <h1>{secondServePercentage}%</h1>
+                </div>
+            </div>
+            <div className={styles.unreturnedRow}>
+                <div className={styles.twoCol}>
+                    <h2>Unreturned Serves</h2>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableCell scope="col" border="bottom">
+                                    Type
+                                </TableCell>
+                                <TableCell scope="col" border="bottom">
+                                    Value
+                                </TableCell>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell scope="row">
+                                    <div className={styles.tableCell}> Aces
+                                    </div>
+                                </TableCell>
+                                <TableCell>
+                                    {+JSON.stringify(state.context.notReturnedServesSecondServes)}
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell scope="row">
+                                    <div className={styles.tableCell}> Not Returned
+                                    </div>
+                                </TableCell>
+                                <TableCell>
+                                    {+JSON.stringify(state.context.notReturnedServesSecondServes)}
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell scope="row">
+                                    <div className={styles.tableCell}> Deuce Side
+                                    </div>
+                                </TableCell>
+                                <TableCell>
+                                    {+JSON.stringify(state.context.notReturnedServesDeuceSide)}
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell scope="row">
+                                    <div className={styles.tableCell}> Ad Side
+                                    </div>
+                                </TableCell>
+                                <TableCell>
+                                    {+JSON.stringify(state.context.notReturnedServesAdSide)}
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell scope="row">
+                                    <div className={styles.tableCell}> First Serves
+                                    </div>
+                                </TableCell>
+                                <TableCell>
+                                    {+JSON.stringify(state.context.notReturnedServesFirstServes)}
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell scope="row">
+                                    <div className={styles.tableCell}> Second Serves
+                                    </div>
+                                </TableCell>
+                                <TableCell>
+                                    {+JSON.stringify(state.context.notReturnedServesSecondServes)}
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+                <div className={styles.servePercentages}>
+                    <Grommet>
+                        <h3>Alley</h3>
+                        <Meter
+                            values={[{
+                                value: percentNotReturnedServesToAlley,
+                            }]}
+                            type="circle"
+                            margin="small"
+                            size="small"
+                            thickness="medium"
+                            color={checkSuccess(percentNotReturnedServesToAlley)}
                         />
-                    </Card>
-                </Col>
-                <Col span={12}>
-                    <Card>
-                        <Statistic
-                            title="Percentage of Points Lost on Serve"
-                            value={percentWonOnServe}
-                            precision={2}
-                            valueStyle={{
-                                color: '#cf1322',
-                            }}
-                            suffix="%"
+                    </Grommet>
+                </div>
+                <div className={styles.servePercentages}>
+                    <Grommet>
+                        <h3>Body</h3>
+                        <Meter
+                            values={[{
+                                value: percentNotReturnedServesToBody,
+                            }]}
+                            type="circle"
+                            margin="small"
+                            size="small"
+                            thickness="medium"
+                            color={checkSuccess(percentNotReturnedServesToBody)}
                         />
-                    </Card>
-                </Col>
-            </Row>
-            <Grommet>
-                <h1>First Serve Percentage</h1>
-                <Meter
-                    values={[{
-                        value: firstServePercentage,
-                    }]}
-                    type="circle"
-                    margin="medium"
-                    size="small"
-                    thickness="medium"
-                />
-                <h1>Second Serve Percentage</h1>
-                <Meter
-                    values={[{
-                        value: secondServePercentage,
-                    }]}
-                    type="circle"
-                    margin="medium"
-                    size="small"
-                    thickness="medium"
-                />
-            </Grommet>
+                    </Grommet>
+                </div>
+                <div className={styles.servePercentages}>
+                    <Grommet>
+                        <h3>Center</h3>
+                        <Meter
+                            values={[{
+                                value: percentNotReturnedServesToCenter,
+                            }]}
+                            type="circle"
+                            margin="small"
+                            size="small"
+                            thickness="medium"
+                            color={checkSuccess(percentNotReturnedServesToCenter)}
+                        />
+                    </Grommet>
+                </div>
+            </div>
         </div>
     );
 };
