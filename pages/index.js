496cb58e67f212fae1de262373c13d359dd0cdc1
morganmarienelson <nelsonmm5047@uwec.edu>
2022-08-03 12:59:05 -0500

    moved out bounce header to only be on home screen

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import {ColorModeSwitcher} from "../components/ColorModeSwitch";
 import React from "react";
 import HomepageNav from "../components/homepageNav";
+import BounceHeading from "../components/layout/BounceHeading";
 
 function HomePage(){
     return(
         <>
+            <BounceHeading/>
             <ColorModeSwitcher/>
             <HomepageNav/>
         </>
