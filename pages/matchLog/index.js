7d710279ba9561d5fdbacc732cf5000b1ad95c4b
morganmarienelson <nelsonmm5047@uwec.edu>
2022-08-05 12:17:16 -0500

    Created match component

--- pages/matchLog/index.js
+++ pages/matchLog/index.js
@@ -3,16 +3,11 @@ import {getSession, signIn} from "next-auth/react";
 import styles from "../../css/matchLog.module.css"
 import {useRouter} from "next/router";
 import BackToHomeHeading from "../../components/statisticsComponents/backToHomeHeading";
-import {Nav} from "grommet";
-import PrevMatchDetailsModal from "../../components/statisticsComponents/prevStatsComponents/prevMatchDetailsModal";
-import {Modal} from "antd";
+import Matches from "../../components/matches";
 
-function StatisticsPage(props){
+function StatisticsPage(){
     const [loading, setLoading] = useState(true)
     const [matches, setMatches] = useState([])
-    const router = useRouter();
-    const [matchDetails, setMatchDetails] = useState([])
-    const [isModalVisible, setIsModalVisible] = useState(false)
 
     const fetchMatches = async () => {
         const response = await fetch('api/matches')
@@ -37,29 +32,6 @@ function StatisticsPage(props){
         return <h2>Loading...</h2>
     }
 
-    const showMatchHandler = (id ) => {
-        router.push('/matchLog/' + id)
-    }
-
-    const showMatchDetails = async (match) =>{
-        setMatchDetails(match);
-        setIsModalVisible(true);
-    }
-
-    const deleteMatch = (id) => {
-        Modal.confirm({
-            title: "Are you sure that you want to delete this match?" ,
-            okType: "danger",
-            onOk: async () => {
-                const response = await fetch(`/api/matches/${id}`, {
-                    method: 'DELETE'
-                })
-                const data = await response.json()
-                fetchMatches();
-            },
-        });
-    }
-
     return(
         <>
         <BackToHomeHeading/>
@@ -67,24 +39,14 @@ function StatisticsPage(props){
             {matches.map((match) => {
                 return (
                         <div key={match.id} className={styles.matchLog}>
-                            <div className={styles.names}>
-                               {match.playerName} vs {match.opponentName}
-                            </div>
-                            <div className={styles.names}>
-                                <Nav direction="row">
-                                    <a onClick={() => showMatchDetails(match)}>View Details</a>
-                                    <a onClick={() => showMatchHandler(match.id)}>View Stats</a>
-                                    <a onClick={() => deleteMatch(match.id)}>Delete</a>
-                                </Nav>
-
-                            </div>
+                            <Matches match={match} fetchMatches={fetchMatches}/>
                         </div>
                 )
             })}
         </div>
-            <PrevMatchDetailsModal matchDetails={matchDetails} prevMatchDetailsModalVisible={isModalVisible} setPrevMatchDetailsModalVisible={setIsModalVisible}/>
         </>
     )
 }
 
 export default StatisticsPage;
+
