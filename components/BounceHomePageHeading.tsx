b29b64116b4ef5fe3ddbbcaa14e5c721fa232374
morganmarienelson <nelsonmm5047@uwec.edu>
2022-08-08 15:53:52 -0500

    next auth editting

--- components/BounceHomePageHeading.tsx
+++ components/BounceHomePageHeading.tsx
@@ -1,39 +1,41 @@
-import {Grommet, Heading} from "grommet/components";
-import {Header, Nav} from 'grommet';
-import styles from "./BounceHeading.module.css"
+import { Grommet, Heading } from "grommet/components";
+import { Header, Nav } from "grommet";
+import styles from "./BounceHeading.module.css";
 import React from "react";
 import Link from "next/link";
-import { signIn, signOut, useSession} from "next-auth/react";
-
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function BounceHomePageHeading() {
-    const { data: session, status: loading } = useSession()
-    return (
-             <Grommet>
-                 <Header className={styles.header}>
-                     <Heading className={styles.headingTitle}>
-                         Bounce
-                     </Heading>
-                     <Nav direction="row">
-                         {!loading && !session && (
-                        <Link href="/api/auth/signin" onClick={e => {
-                            signIn('github')
-                        }}>
-                            <a className={styles.a}>Sign In</a>
-                        </Link>
-                         )}
-                         { session && (
-                         <Link href="/api/auth/signout" onClick={e => {
-                             signOut()
-                         }}>
-                             <a className={styles.a}>Sign Out</a>
-                         </Link>
-                         )}
-                     </Nav>
-              </Header>
-              </Grommet>
-        )
-
+  const { data: session } = useSession();
+  return (
+    <Grommet>
+      <Header className={styles.header}>
+        <Heading className={styles.headingTitle}>Bounce</Heading>
+        <Nav direction="row">
+          {!session && (
+            <Link
+              href="/api/auth/signin"
+              onClick={(e) => {
+                signIn("github");
+              }}
+            >
+              <a className={styles.a}>Sign In</a>
+            </Link>
+          )}
+          {session && (
+            <Link
+              href="/api/auth/signout"
+              onClick={(e) => {
+                signOut();
+              }}
+            >
+              <a className={styles.a}>Sign Out</a>
+            </Link>
+          )}
+        </Nav>
+      </Header>
+    </Grommet>
+  );
 }
 
 export default BounceHomePageHeading;
--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -4,8 +4,8 @@ import GitHubProvider from "next-auth/providers/github"
 export default NextAuth({
     providers: [
         GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+            clientId: process.env.GITHUB_ID ?? "",
+            clientSecret: process.env.GITHUB_SECRET ?? "",
         }),
     ],
     database: process.env.DATABASE_URL,
