969dfe655ce8d1eb747421a1344dfe8984c37c02
morganmarienelson <nelsonmm5047@uwec.edu>
2022-08-09 15:22:35 -0500

    fixed use effect error

--- components/statisticsComponents/newStatsComponents/saveMatchPanel.tsx
+++ components/statisticsComponents/newStatsComponents/saveMatchPanel.tsx
@@ -1,86 +1,87 @@
-import {Button, Modal} from "antd";
+import { Button, Modal } from "antd";
 import styles from "../../../css/statistics/savePanel.module.scss";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import SaveMatchModal from "./saveMatchModal";
-import {getSession} from "next-auth/react";
-import {useRouter} from "next/router";
-import {MatchStats} from "../../../types/matchStats";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { MatchStats } from "../../../types/matchStats";
 
-interface SaveMatchPanelProps{
-    state: MatchStats;
+interface SaveMatchPanelProps {
+  state: MatchStats;
 }
 
-const SaveMatchPanel: React.FC<SaveMatchPanelProps> = ({state}) => {
-    const [showSaveMatchModal, setShowSaveMatchModal] = useState(false);
-    const [signedIn, setSignedIn] = useState(false)
-    const router = useRouter();
+const SaveMatchPanel: React.FC<SaveMatchPanelProps> = ({ state }) => {
+  const [showSaveMatchModal, setShowSaveMatchModal] = useState(false);
+  const [signedIn, setSignedIn] = useState(false);
+  const router = useRouter();
 
-    useEffect(() => {
-        const securePage = async () => {
-            const session = await getSession();
-            if (!session) {
-                setSignedIn(false);
-            } else {
-                setSignedIn(true);
-            }
-        }
-        securePage();
-    }, )
-
-    const confirmExit = () => {
-        if (signedIn){
-            Modal.confirm({
-                title: "Are you sure that you want to go to the home screen? This match has not been saved.",
-                okType: "danger",
-                onOk: () => {
-                    router.push("/");
-                },
-            });
-        } else {
-            router.push("/");
-        }
-
+  useEffect(() => {
+    const securePage = async () => {
+      const session = await getSession();
+      if (!session) {
+        setSignedIn(false);
+      } else {
+        setSignedIn(true);
+      }
     };
+    securePage();
+  }, []);
 
-    const saveMatch = () => {
-            setShowSaveMatchModal(true);
+  const confirmExit = () => {
+    if (signedIn) {
+      Modal.confirm({
+        title:
+          "Are you sure that you want to go to the home screen? This match has not been saved.",
+        okType: "danger",
+        onOk: () => {
+          router.push("/");
+        },
+      });
+    } else {
+      router.push("/");
     }
+  };
 
-    return (
-        <div className={styles.btnContainer}>
-            <div className={styles.btn}>
-            <Button
-                className={styles.homeBtn}
-                type="primary"
-                style={{
-                    background: "#480096",
-                    border: "#480096",
-                }}
-                onClick={confirmExit}
-            >
-                <div className={styles.btnTitle}>Back to Home</div>
-            </Button>
-            </div>
-            {signedIn && (
-            <div className={styles.btn}>
-            <Button
-                className={styles.homeBtn}
-                type="primary"
-                style={{
-                    background: "#480096",
-                    border: "#480096",
-                }}
-                onClick={saveMatch}
-            >
+  const saveMatch = () => {
+    setShowSaveMatchModal(true);
+  };
 
-                <div className={styles.btnTitle}> Save Match </div>
-            </Button>
-            </div>
-                )}
-            <SaveMatchModal setShowModal={setShowSaveMatchModal} showModal={showSaveMatchModal} />
+  return (
+    <div className={styles.btnContainer}>
+      <div className={styles.btn}>
+        <Button
+          className={styles.homeBtn}
+          type="primary"
+          style={{
+            background: "#480096",
+            border: "#480096",
+          }}
+          onClick={confirmExit}
+        >
+          <div className={styles.btnTitle}>Back to Home</div>
+        </Button>
+      </div>
+      {signedIn && (
+        <div className={styles.btn}>
+          <Button
+            className={styles.homeBtn}
+            type="primary"
+            style={{
+              background: "#480096",
+              border: "#480096",
+            }}
+            onClick={saveMatch}
+          >
+            <div className={styles.btnTitle}> Save Match </div>
+          </Button>
         </div>
-
-    )
-}
+      )}
+      <SaveMatchModal
+        setShowModal={setShowSaveMatchModal}
+        showModal={showSaveMatchModal}
+      />
+    </div>
+  )
+};
 
 export default SaveMatchPanel;
