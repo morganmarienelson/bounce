a9f315ab9b4e3ae2d59afe499080f54b62fdec70
Morgan Nelson <nelsonmm5047@uwec.edu>
2023-09-05 15:39:28 -0500

    route created

--- pages/api/user/route.ts
+++ pages/api/user/route.ts
@@ -1,9 +1,38 @@
 import {NextResponse} from "next/server";
+import {db} from "../../../lib/db";
+import {hash} from "bcrypt";
 
 export async function POST(req: Request) {
    try {
-       const body = req.json()
-       return NextResponse.json(body)
+       const body = req.json();
+       const {email, password, userName, firstName, lastName} = body;
+
+        const existingUserByEmail = await db.user.findUnique({
+            where: {email: email}
+        });
+        if (existingUserByEmail){
+            return NextResponse.json({user: null, message: "There is already an account associated with this email address"}, {status: 409});
+        }
+
+       const existingUserByUsername = await db.user.findUnique({
+           where: {userName: userName}
+       });
+       if (existingUserByUsername){
+           return NextResponse.json({user: null, message: "This username already exists"}, {status: 409});
+       }
+
+
+        const hashedPassword = await hash(password, 10);
+       const newUser = await db.user.create({
+           data: {
+               userName,
+               email,
+               password: hashedPassword,
+               firstName,
+               lastName
+           }
+       })
+       return NextResponse.json({user: newUser, message: "User was created successfully"}, {status: 201})
    } catch(error){
    }
 }
